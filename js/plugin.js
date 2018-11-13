c643e072e13dfda5773c62a0da102b6dee468827
Amal <Amallhossamy@gmail.com>
2018-11-14 00:17:27 +0200

    edit size

--- js/plugin.js
+++ js/plugin.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('.collapse.in').prev('.panel-heading').addClass('active');
     $('#accordion, #bs-collapse')
         .on('show.bs.collapse', function (a) {
@@ -7,4 +7,14 @@ $(document).ready(function () {
         .on('hide.bs.collapse', function (a) {
             $(a.target).prev('.panel-heading').removeClass('active');
         });
+});
+*/
+
+$(function () {
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 });
